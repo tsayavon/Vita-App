2bac733557f8b62cb12822249491be01ec74867c
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-04 18:29:24 -1000

    update and delete added

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('posts/', views.posts_index, name='index'),
     path('posts/<int:post_id>/', views.posts_detail, name='detail'),
     path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
-
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -24,3 +24,10 @@ class PostCreate(CreateView):
   fields = ('title', 'location', 'description')
   success_url = '/posts/'
 
+class PostUpdate(UpdateView):
+  model = Post
+  fields = ('title', 'location', 'description')
+
+class PostDelete(DeleteView):
+  model = Post
+  success_url = '/posts/'
