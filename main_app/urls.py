44f1b1df87d9a1a02b1e4fede697fe93793f1401
Adrian Maningding <amazingadrian@Adrians-MacBook-Pro.local>
2022-10-11 20:17:00 -0700

    Photo Upload Finished

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
     path('posts/<int:post_id>/add_category/', views.add_category, name='add_category'),
+    path('posts/<int:post_id>/add_photo/', views.add_photo, name='add_photo'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
